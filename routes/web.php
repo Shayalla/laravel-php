fce23393bfeaaae3ce06d3b369a6f923a1f7eec1
Jessie <miss_sc_jessi@hotmail.com>
2022-05-11 17:32:49 -0300

    realizando logicas

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $nome = "Matheus";
+    $idade = 29;
+
+    $arr = [10,20,30,40,50];
+    $nomes = ['joao', 'maria', 'jose'];
+    return view('welcome',
+        [
+            'nome' => $nome,
+            'idade' => $idade,
+            'profissao' => "Programador",
+            'arr' => $arr,
+            'nomes' => $nomes
+        ]);
 });
